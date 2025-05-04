ec8b05d4628ca34cbfa9dee4b92a72f3145b409c
Nikita Kuritsyn <martyz89@yandex.ru>
2025-05-04 14:38:00 +0300

    fix otp expire job

--- src/main/java/ru/random/walk/authservice/service/job/OneTimePasswordExpireJob.java
+++ src/main/java/ru/random/walk/authservice/service/job/OneTimePasswordExpireJob.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.random.walk.authservice.repository.OneTimePasswordRepository;
 
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ public class OneTimePasswordExpireJob implements Job {
     private final OneTimePasswordRepository oneTimePasswordRepository;
 
     @Override
+    @Transactional
     public void execute(JobExecutionContext context) {
         log.info("Start executing OneTimePasswordExpireJob");
         int deletedCount = oneTimePasswordRepository.deleteAllByExpiresAtBefore(LocalDateTime.now());
